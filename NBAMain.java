30083d1fd69e74a84caa22c09a37b873312460a4
Mike-Flans <103156163+Mike-Flans@users.noreply.github.com>
2022-05-01 16:55:04 -0400

    Add files via upload

---
+++ NBAMain.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class NBAMain extends Application
+{
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+   
+   @Override
+   public void start(Stage stage) throws Exception
+   {
+      Parent root = FXMLLoader.load(getClass().getResource("NBAView.fxml"));
+      Scene scene = new Scene(root);
+      stage.setTitle("NBA Player Info");
+      stage.setScene(scene);
+      stage.show();
+   }
+}
