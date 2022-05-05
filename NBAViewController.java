f83851ff886cf5765088d69d79f38de1a918cb18
Mike-Flans <103156163+Mike-Flans@users.noreply.github.com>
2022-05-04 21:06:42 -0400

    Add files via upload

---
+++ NBAViewController.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class NBAViewController {
+
+    @FXML
+    private Label heightLabel;
+
+    @FXML
+    private Label myLabel1;
+
+    @FXML
+    private Label myLabel2;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label teamLabel;
+
+    @FXML
+    private Label weightLabel;
+
+    @FXML
+    void searchPlayer(ActionEvent event) {
+
+    }
+
+}
